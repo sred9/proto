1910fc573978b81c783e83eb879a89cf0158b2ef
Sreehas Dommata <sreehasdommata98@gmail.com>
2018-07-07 13:11:34 +0530

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,3 +1,3 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/sred9/pen/jZYOrE.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/sred9/pen/WyzKZB.
 
- project, my friend and i've been working using bootstrap 4
+ 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,197 @@
-function scrollNav(){$('.nav a').click(function(){$(".active").removeClass("active");$(this).closest('li').addClass("active");var theClass=$(this).attr("class");$('.'+theClass).parent('li').addClass('active');$('html, body').stop().animate({scrollTop:$($(this).attr('href')).offset().top-160},400);return!1});$('.scrollTop a').scrollTop()}
-scrollNav();(function($){$.fn.countTo=function(options){options=options||{};return $(this).each(function(){var settings=$.extend({},$.fn.countTo.defaults,{from:$(this).data('from'),to:$(this).data('to'),speed:$(this).data('speed'),refreshInterval:$(this).data('refresh-interval'),decimals:$(this).data('decimals')},options);var loops=Math.ceil(settings.speed/settings.refreshInterval),increment=(settings.to-settings.from)/loops;var self=this,$self=$(this),loopCount=0,value=settings.from,data=$self.data('countTo')||{};$self.data('countTo',data);if(data.interval){clearInterval(data.interval)}
-data.interval=setInterval(updateTimer,settings.refreshInterval);render(value);function updateTimer(){value+=increment;loopCount++;render(value);if(typeof(settings.onUpdate)=='function'){settings.onUpdate.call(self,value)}
-if(loopCount>=loops){$self.removeData('countTo');clearInterval(data.interval);value=settings.to;if(typeof(settings.onComplete)=='function'){settings.onComplete.call(self,value)}}}
-function render(value){var formattedValue=settings.formatter.call(self,value,settings);$self.html(formattedValue)}})};$.fn.countTo.defaults={from:0,to:0,speed:1000,refreshInterval:100,decimals:0,formatter:formatter,onUpdate:null,onComplete:null};function formatter(value,settings){return value.toFixed(settings.decimals)}}(jQuery));jQuery(function($){$('.count-number').data('countToOptions',{formatter:function(value,options){return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,',')}});$('.timer').each(count);function count(options){var $this=$(this);options=$.extend({},options||{},$this.data('countToOptions')||{});$this.countTo(options)}});var TxtRotate=function(el,toRotate,period){this.toRotate=toRotate;this.el=el;this.loopNum=0;this.period=parseInt(period,10)||2000;this.txt='';this.tick();this.isDeleting=!1};TxtRotate.prototype.tick=function(){var i=this.loopNum%this.toRotate.length;var fullTxt=this.toRotate[i];if(this.isDeleting){this.txt=fullTxt.substring(0,this.txt.length-1)}else{this.txt=fullTxt.substring(0,this.txt.length+1)}
-this.el.innerHTML='<span class="wrap">'+this.txt+'</span>';var that=this;var delta=300-Math.random()*100;if(this.isDeleting){delta/=2}
-if(!this.isDeleting&&this.txt===fullTxt){delta=this.period;this.isDeleting=!0}else if(this.isDeleting&&this.txt===''){this.isDeleting=!1;this.loopNum++;delta=500}
-setTimeout(function(){that.tick()},delta)};window.onload=function(){var elements=document.getElementsByClassName('txt-rotate');for(var i=0;i<elements.length;i++){var toRotate=elements[i].getAttribute('data-rotate');var period=elements[i].getAttribute('data-period');if(toRotate){new TxtRotate(elements[i],JSON.parse(toRotate),period)}}
-var css=document.createElement("style");css.type="text/css";css.innerHTML=".txt-rotate > .wrap { border-right: 0.08em solid #666 }";document.body.appendChild(css)};window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){document.getElementById("navbar").style.top="0"}else{document.getElementById("navbar").style.top="-50px"}}
+$("[data-toggle=popover]").popover();
+
+$('.popover-dismiss').popover({
+  trigger: 'focus'
+});
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(document).ready(function () {
+    $('.code-box-copy').codeBoxCopy();
+});
+
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
+
+(function ($) {
+	$.fn.countTo = function (options) {
+		options = options || {};
+		
+		return $(this).each(function () {
+			// set options for current element
+			var settings = $.extend({}, $.fn.countTo.defaults, {
+				from:            $(this).data('from'),
+				to:              $(this).data('to'),
+				speed:           $(this).data('speed'),
+				refreshInterval: $(this).data('refresh-interval'),
+				decimals:        $(this).data('decimals')
+			}, options);
+			
+			// how many times to update the value, and how much to increment the value on each update
+			var loops = Math.ceil(settings.speed / settings.refreshInterval),
+				increment = (settings.to - settings.from) / loops;
+			
+			// references & variables that will change with each update
+			var self = this,
+				$self = $(this),
+				loopCount = 0,
+				value = settings.from,
+				data = $self.data('countTo') || {};
+			
+			$self.data('countTo', data);
+			
+			// if an existing interval can be found, clear it first
+			if (data.interval) {
+				clearInterval(data.interval);
+			}
+			data.interval = setInterval(updateTimer, settings.refreshInterval);
+			
+			// initialize the element with the starting value
+			render(value);
+			
+			function updateTimer() {
+				value += increment;
+				loopCount++;
+				
+				render(value);
+				
+				if (typeof(settings.onUpdate) == 'function') {
+					settings.onUpdate.call(self, value);
+				}
+				
+				if (loopCount >= loops) {
+					// remove the interval
+					$self.removeData('countTo');
+					clearInterval(data.interval);
+					value = settings.to;
+					
+					if (typeof(settings.onComplete) == 'function') {
+						settings.onComplete.call(self, value);
+					}
+				}
+			}
+			
+			function render(value) {
+				var formattedValue = settings.formatter.call(self, value, settings);
+				$self.html(formattedValue);
+			}
+		});
+	};
+	
+	$.fn.countTo.defaults = {
+		from: 0,               // the number the element should start at
+		to: 0,                 // the number the element should end at
+		speed: 1000,           // how long it should take to count between the target numbers
+		refreshInterval: 100,  // how often the element should be updated
+		decimals: 0,           // the number of decimal places to show
+		formatter: formatter,  // handler for formatting the value before rendering
+		onUpdate: null,        // callback method for every time the element is updated
+		onComplete: null       // callback method for when the element finishes updating
+	};
+	
+	function formatter(value, settings) {
+		return value.toFixed(settings.decimals);
+	}
+}(jQuery));
+
+jQuery(function ($) {
+  // custom formatting example
+  $('.count-number').data('countToOptions', {
+	formatter: function (value, options) {
+	  return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+	}
+  });
+  
+  // start all the timers
+  $('.timer').each(count);  
+  
+  function count(options) {
+	var $this = $(this);
+	options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+	$this.countTo(options);
+  }
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $('.code-box-copy').codeBoxCopy({
+        tooltipText: 'Copied',
+        tooltipShowTime: 1000,
+        tooltipFadeInTime: 300,
+        tooltipFadeOutTime: 300
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function selectText(id){
+    var sel, range;
+    var el = document.getElementById(id); //get element id
+    if (window.getSelection && document.createRange) { //Browser compatibility
+      sel = window.getSelection();
+      if(sel.toString() == ''){ //no text selection
+         window.setTimeout(function(){
+            range = document.createRange(); //range object
+            range.selectNodeContents(el); //sets Range
+            sel.removeAllRanges(); //remove all ranges from selection
+            sel.addRange(range);//add Range to a Selection.
+        },1);
+      }
+    }else if (document.selection) { //older ie
+        sel = document.selection.createRange();
+        if(sel.text == ''){ //no text selection
+            range = document.body.createTextRange();//Creates TextRange object
+            range.moveToElementText(el);//sets Range
+            range.select(); //make selection.
+        }
+    }
+}
